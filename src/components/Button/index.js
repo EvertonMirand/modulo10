8fab14a7834711066c8245310fbe91ed84662cda
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2019-10-08 19:08:22 -0300

    Remove erro ao rodar projeto no iOS

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,11 +2,10 @@ import React, { forwardRef } from 'react';
 
 import { ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
-import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Text } from './styles';
 
-export default function Button({ children, loading, ...rest }, ref) {
+export default function Button({ children, loading, ...rest }) {
   return (
     <Container {...rest}>
       {loading ? (
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -10,7 +10,7 @@ export const Container = styled(RectButton)`
   align-items: center;
 `;
 
-export const TInput = styled.Text`
+export const Text = styled.Text`
   color: #fff;
   font-weight: bold;
   font-size: 16px;
